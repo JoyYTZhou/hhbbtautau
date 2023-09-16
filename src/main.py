60d36da9e8aef1c0f1003842c4bc694238f859ed
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2023-09-15 23:20:36 -0400

    some udpate on selections

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+# UPDATE TIME: 2023-09-15
+# FROM JOY
 import awkward as ak
-from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema, BaseSchema
 import argparse
 from tqdm import tqdm
 import glob
+import numpy as np
 
+# TODO: Change this to use cfg
 parser = argparse.ArgumentParser(
                     prog='looper',
                     description='loops over the events directory and performs selections',
@@ -16,4 +20,9 @@ args=parser.parse_args()
 if args.isDirectory==True:
     filelist=glob.glob(args.filename+"/**/*.root", recursive=True)
     
+# TODO: Change this later so that metadata tag can be extracted from cfg file to include sample type
+events = NanoEventsFactory.from_root(
+    filelist,
+    schemeclass=BaseSchema,
+    metadata={"dataset": "ttbar"},).events()
 
