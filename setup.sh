8f56256f9a53f6b46f265bacb846ebf56a1873d8
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2023-11-06 19:32:50 -0500

    added file output functionality

--- setup.sh
+++ setup.sh
@@ -2,4 +2,13 @@
 
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 export HHBBTT=$PWD
+
+# MUST set the path to where you store the output before running this script
 export OUTPUTPATH=$DATAPATH/output
+
+rm -r $OUTPUTPATH
+mkdir $OUTPUTPATH
+mkdir $OUTPUTPATH/cutflow
+mkdir $OUTPUTPATH/object
+
+
--- src/analysis/processing.py
+++ src/analysis/processing.py
@@ -10,9 +10,10 @@ from analysis.selutility import lepton_selections, pair_selections, jet_selectio
 from analysis.dsmethods import *
 from analysis.histbooker import hbbtautau_accumulate, hhtobbtautau_accumulator
 
+
 def trigger_selection(selection, events, cfg):
     """Add trigger selections to coffea processor selection.
-    
+
     :param events: events from root NANOAOD files
     :type events: coffea.nanoevents.methods.base.NanoEventsArray
     :param selection: selection critiera in boolean mask vectors in a compact manner
@@ -25,11 +26,10 @@ def trigger_selection(selection, events, cfg):
     return 0
 
 
-
 class hhbbtautauProcessor(processor.ProcessorABC):
     def __init__(self, blind=True):
-        self._year=None
-        self._blind=False
+        self._year = None
+        self._blind = False
         self._configure()
         self._accumulator = hhtobbtautau_accumulator(cfg)
         self._dataset = None
@@ -41,9 +41,9 @@ class hhbbtautauProcessor(processor.ProcessorABC):
     def _configure(self, events=None):
         """Configure the processor."""
 
-        cfg.DYNACONF_WORKS="merge_configs"
-        cfg.MERGE_ENABLED_FOR_DYNACONF=True
-        
+        cfg.DYNACONF_WORKS = "merge_configs"
+        cfg.MERGE_ENABLED_FOR_DYNACONF = True
+
         if events is not None:
             self._dataset = events.metadata['dataset']
 
@@ -54,14 +54,14 @@ class hhbbtautauProcessor(processor.ProcessorABC):
             "Objects": dict_accumulator(object_accs)
         })
         """
-        
+
         if not cfg.size:
             return self.accumulator.identity()
         self._configure(events)
 
         # Triggers
         # TODO: Add triggers
-        
+
         # Lepton selections
         events_dict, cutflow_dict, object_dict = lepton_selections(events, cfg)
         # Pair selections
@@ -76,8 +76,12 @@ class hhbbtautauProcessor(processor.ProcessorABC):
         return {self._dataset: output}
 
     def postprocess(self, acc_output):
-        # TODO: Simplify this acc_output  
-        pass
+        # TODO: Simplify this acc_output
+        for dataset in acc_output.keys():
+            for channelname, output in acc_output[dataset].items():
+                acc_output[dataset][channelname]["Objects"] = {kinematic: col_acc.value if col_acc.value.size else [np.nan]
+                                                               for kinematic, col_acc in output['Objects'].items()}
+
 
 def output_export(acc_output, rt_cfg):
     """ Export the accumulator to csv file based on settings in the run configuration
@@ -99,26 +103,28 @@ def output_export(acc_output, rt_cfg):
     obj_df_list = init_output(cfg.signal.channelnames)
     for dataset, output in acc_output.items():
         for channelname, acc in output.items():
-            cf_df_list[channelname].append(pd.from_dict(acc['Cutflow']))
-            obj_df = pd.from_dict(acc['Object']) 
+            cf_df_list[channelname].append(pd.DataFrame.from_dict(
+                acc['Cutflow'], orient='index', columns=[dataset]))
+            obj_df = pd.DataFrame.from_dict(
+                acc['Objects'], orient='index').transpose()
             obj_df['Dataset'] = dataset
             obj_df_list[channelname].append(obj_df)
-    concat_output(cf_df_list, os.path.join(rt_cfg.outputdir_path, "cutflow"), acc_output.keys())
-    concat_output(obj_df_list, os.path.join(rt_cfg.outputdir_path, "object"))
+    concat_output(cf_df_list, axis=1, dir_name=os.path.join(
+        rt_cfg.outputdir_path, "cutflow"))
+    concat_output(obj_df_list, axis=0, dir_name=os.path.join(
+        rt_cfg.outputdir_path, "object"))
+
 
 def init_output(channelnames):
-    cf_df_list = {} 
-    cf_df_list.fromkeys(channelnames, [])
+    cf_df_list = {channel: [] for channel in channelnames}
     return cf_df_list
 
-def concat_output(cf_df_list, dir_name = None, index = None):
+
+def concat_output(cf_df_list, axis=0, dir_name=None, index=None):
     for channelname, df_list in cf_df_list.items():
-        cf_df_list[channelname] = pd.concat(df_list)
+        cf_df_list[channelname] = pd.concat(df_list, axis=axis)
         if index is not None:
             cf_df_list[channelname].index = index
         if dir_name is not None:
             finame = f"{channelname}.csv"
-            cf_df_list[channelname].to_csv(os.path.join(dir_name, finame)) 
-
-     
-    
+            cf_df_list[channelname].to_csv(os.path.join(dir_name, finame))
