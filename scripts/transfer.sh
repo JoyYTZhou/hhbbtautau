2d683fec978b6a1800347360d50db5fde0676e7f
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2023-11-13 19:40:34 -0500

    ready for batch submission

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -7,18 +7,10 @@ OUTDIR=root://cmseos.fnal.gov//store/user/username/MyCondorOutputArea/
 echo "xrdcp output for condor to "
 echo $CONDORPATH
 
+src=$OUTPUTPATH
+dest=$CONDORPATH
 
-for FILE in
-do
-    echo "xrdcp -f ${FILE} ${OUTDIR}/${FILE}"
-    echo "${FILE}"
-    echo "${OUTDIR}"
-    xrdcp -f ${FILE} ${OUTDIR}/${FILE} 2>&1
-          XRDEXIT=$?
-               if [[ $XRDEXIT -ne 0 ]]; then
-                       rm *.root
-                           echo "exit code $XRDEXIT, failure in xrdcp"
-                               exit $XRDEXIT
-                                 fi
-                                   rm ${FILE}
-                               done
+find "$src" -type f | while read -r file; do
+    relpath="${file#$src}"
+    xrdcp "$file" "$dest$relpath"
+done
--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
+# =================================================================
+# This script sets up the environment in any execution machine
+# =================================================================
 
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 export HHBBTT=$PWD
 
 # MUST set the path $OUTPUTPATH to where you store the output before running this script
 
+# If not using batch script
 if [ -z "${IS_CONDOR}" ]; then
     rm -rv $OUTPUTPATH/cutflow
     rm -rv $OUTPUTPATH/object
     mkdir -pv $OUTPUTPATH/cutflow
     mkdir -pv $OUTPUTPATH/object
+# if using batch script
 else
     if [ ! -z "$1" ]; then
         rm -rv $OUTPUTPATH/$1/cutflow
@@ -19,9 +24,6 @@ else
         echo "full output directory on executation area is $OUTPUTPATH"
     else
         export OUTPUTPATH=$OUTPUTPATH/backup
-        eosrm -r $SHORTPATH/backup
-        eosmkdir -p $SHORTPATH/backup/cutflow
-        eosmkdir -p $SHORTPATH/backup/object
     fi
 fi
 
