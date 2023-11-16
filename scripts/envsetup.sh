2d7e540a790e70df6ad3773076797f1f8451ceeb
Joy Zhou <joyzhou@cmslpc129.fnal.gov>
2023-11-15 21:11:10 -0600

    update shell scripts

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -2,7 +2,11 @@
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_103swan/x86_64-centos7-gcc11-opt/setup.sh
 python -m venv hhbbttrun
+echo "creating new venv..."
 source hhbbttrun/bin/activate
-export PYTHONPATH=${ENVNAME}/lib/python3.9/site-packages:$PYTHONPATH
-python3 -m pip install coffea --upgrade
-python3 -m pip install dynaconf
+export ENV_NAME=hhbbttrun
+python3 -m pip install coffea --upgrade --no-cache-dir
+echo "Installed Coffea"
+python3 -m pip install dynaconf --no-cache-dir
+echo "Installed dyanconf"
+export PYTHONPATH=$ENV_NAME/lib/python3.9/site-packages:$PYTHONPATH
