65957489c82e4f635ffa5e5a4d65442cb1f7458c
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2023-11-19 19:35:48 -0500

    update vom proxy check

---
+++ scripts/vomcheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if VOMS proxy exists
+if voms-proxy-info --exists; then
+  echo "VOMS proxy exists."
+
+  # Check if the VOMS proxy is valid
+  if voms-proxy-info --valid 2>&1 > /dev/null; then
+    echo "VOMS proxy is valid."
+    # Optionally, you can display time left for the proxy
+    timeleft=$(voms-proxy-info --timeleft)
+    echo "Time left for the proxy: ${timeleft} seconds."
+  else
+    echo "VOMS proxy is not valid or has expired."
+  fi
+else
+  echo "No VOMS proxy found."
+fi
