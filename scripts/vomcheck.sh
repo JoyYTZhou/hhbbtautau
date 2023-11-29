cbb4edddedfd9c56dc824603068cf74b9f0197ae
Joy Zhou <joyzhou@cmslpc143.fnal.gov>
2023-11-28 20:28:08 -0600

    simplify condor scripts

--- scripts/vomcheck.sh
+++ scripts/vomcheck.sh
@@ -5,7 +5,7 @@ if voms-proxy-info --exists; then
   echo "VOMS proxy exists."
 
   # Check if the VOMS proxy is valid
-  if voms-proxy-info --valid 2>&1 > /dev/null; then
+  if voms-proxy-info --valid 10 2>&1 > /dev/null; then
     echo "VOMS proxy is valid."
     # Optionally, you can display time left for the proxy
     timeleft=$(voms-proxy-info --timeleft)
