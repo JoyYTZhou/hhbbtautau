e77b10fe0f56909a81423b0f8f4cceb8f4df4b60
Yuntong Zhou <yuntongzhou@yuntongs-air.wifi.local.cmu.edu>
2023-12-07 13:21:24 -0500

    add runtime error handling

--- src/analysis/runtask.py
+++ src/analysis/runtask.py
@@ -8,6 +8,7 @@ from analysis.processing import hhbbtautauProcessor
 from analysis.dsmethods import extract_process
 from analysis.histbooker import accumulate_dicts
 import pandas as pd
+import re
 
 def run_single(filename, process_name):
     """Run the processor on a single file.
@@ -49,6 +50,45 @@ def rerun_exceptions(exceptions):
             del out
     return exception_out
             
+def future_wrapper(fileset, rs):
+    """Wrapper around the futures executor to handle the case where the job fails due to an XRootD error.
+
+    :param fileset: fileset
+    :type fileset: dict
+    :param rs: run settings
+    :type rs: DynaConf object
+    :return: output (still accumulatable)
+    :rtype: dict_accumulator
+    """
+    futures_run = processor.Runner(
+            executor=processor.FuturesExecutor(
+                compression=rs.COMPRESSION,
+                workers=rs.WORKERS,
+                recoverable=rs.RECOVERABLE,
+                merging=(rs.N_BATCHES, rs.MIN_SIZE, rs.MAX_SIZE)
+            ),
+            schema=BaseSchema,
+            chunksize=rs.CHUNK_SIZE,
+            xrootdtimeout=rs.TIMEOUT
+        )
+    while True:
+        try: 
+            out, exceptions = futures_run(
+                fileset,
+                treename=rs.TREE_NAME,
+                processor_instance=hhbbtautauProcessor(),
+            )
+            out.add(rerun_exceptions(exceptions))
+            break
+        except RuntimeError as e:
+            filename = re.search(r"Metadata for file (.*?) could not be accessed", str(e)).group(1)
+            print(f"An error occurred with file {filename}: {e}")
+            for dataset in fileset:
+                if filename in fileset[dataset]:
+                    fileset[dataset].remove(filename)
+                    break
+    return out
+
 def run_jobs(fileset, rs):
     """Run the processor on a fileset.
     
@@ -60,23 +100,7 @@ def run_jobs(fileset, rs):
     :rtype: dict_accumulator
     """
     if rs.RUN_MODE == "future":
-        futures_run = processor.Runner(
-            executor=processor.FuturesExecutor(
-                compression=rs.COMPRESSION,
-                workers=rs.WORKERS,
-                recoverable=rs.RECOVERABLE,
-                merging=(rs.N_BATCHES, rs.MIN_SIZE, rs.MAX_SIZE)
-            ),
-            schema=BaseSchema,
-            chunksize=rs.CHUNK_SIZE,
-            xrootdtimeout=rs.TIMEOUT
-        )
-        out, exceptions = futures_run(
-            fileset,
-            treename=rs.TREE_NAME,
-            processor_instance=hhbbtautauProcessor(),
-        )
-        out.add(rerun_exceptions(exceptions))
+       out = future_wrapper(fileset, rs) 
     elif rs.RUN_MODE == "iterative":
         iterative_run = processor.Runner(
             executor=processor.IterativeExecutor(
@@ -94,4 +118,6 @@ def run_jobs(fileset, rs):
         out = None
     else:
         raise TypeError("Unknown run mode: %s" % rs.RUN_MODE)
+
+
     return out
