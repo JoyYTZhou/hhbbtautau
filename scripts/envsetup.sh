5f922f2932ac24c7b92dd35b00ea6d42d4e698d2
Joy Zhou <joyzhou@cmslpc171.fnal.gov>
2024-01-04 16:38:48 -0600

    prefix change

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -8,13 +8,15 @@ python -m venv hhbbttrun
 echo "creating new venv..."
 source hhbbttrun/bin/activate
 export ENV_NAME=hhbbttrun
-python3 -m pip install coffea --upgrade --no-cache-dir
+cd ~/nobackup/AnalysisUtil
+python3 -m pip install -e .
 echo "Installed Coffea"
 python3 -m pip install dynaconf --no-cache-dir
 echo "Installed dyanconf"
 python3 -m pip install vector --upgrade --no-cache-dir
 echo "Installed vector"
 deactivate
+cd ~/nobackup
 
 # create tarball
 tar -czf ~/work/hhbbtautau/${ENV_NAME}.tar.gz ${ENV_NAME}
