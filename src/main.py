3536d22c0bcc9c020f41ac977766e725a411ea44
Joy Zhou <joyzhou@cmslpc141.fnal.gov>
2024-02-03 15:40:07 -0600

    memory test 2/10 success

--- src/main.py
+++ src/main.py
@@ -7,16 +7,8 @@ from analysis.selutility import *
 proc = Processor(rs)
 
 if rs.TEST_MODE:
-    proc.setdata()
-    filename, partitions = list(proc.data.items())[0]
-    # print("processing filename, partitions: ", filename, partitions)
-    # passed, cf = proc.singlerun({filename: partitions}, suffix="_test")
-    passed, cf = proc.singlerun({filename: "Events"}, suffix="_test")
-    print("computing...")
-    cf_df = proc.res_to_df(cf)
-    print("writing to csv...")
-    cf_df.to_csv(pjoin(proc.outdir, "cutflow.csv"))
-else:
     proc.runmultiple(0,1)
+else:
+    proc.runmultiple()
 
 
