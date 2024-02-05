e51887c407744a0a2daddfd5d8c30be93d161f66
Joy Zhou <joyzhou@cmslpc166.fnal.gov>
2024-02-05 14:01:20 -0600

    dask test 2/10

--- scripts/jupenvsetup.sh
+++ scripts/jupenvsetup.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
-cd ~/work/hhbbtautau
-rm -r *.tar.gz
-source scripts/sasetup.sh
+#!/usr/bin/bash
+source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos7-gcc11-opt/setup.sh
 cd ~/nobackup
-export ENV_NAME=newcoffea
+export ENV_NAME=coffeajup
 python -m venv ${ENV_NAME}
 echo "creating new venv..."
 source ${ENV_NAME}/bin/activate
-python3 -m pip install coffea
+
+python3 -m pip install coffea --upgrade --no-cache-dir
 echo "Installed Coffea"
 python3 -m pip install dynaconf --no-cache-dir
 echo "Installed dyanconf"
@@ -16,6 +14,4 @@ python3 -m pip install vector --upgrade --no-cache-dir
 echo "Installed vector"
 deactivate
 
-# create tarball
-tar -czf ~/work/hhbbtautau/${ENV_NAME}.tar.gz ${ENV_NAME}
-cd ~/work/hhbbtautau
+
