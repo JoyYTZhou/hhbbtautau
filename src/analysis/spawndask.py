a10b4afd7017de3d0854a78dbc83a728d5f6e797
Joy Zhou <joyzhou@cmslpc202.fnal.gov>
2024-02-14 20:10:19 -0600

    update config

--- src/analysis/spawndask.py
+++ src/analysis/spawndask.py
@@ -5,13 +5,32 @@ from analysis.helper import *
 from config.selectionconfig import runsetting as rs
 import dask.config
 
+def spawnclient():
+    if not rs.IS_CONDOR:
+        client = spawnLocal()
+    else:
+        process=rs.PROCESS_NAME
+        if rs.SUBMIT_DASK: client = spawnCondor()
+    return client 
+
 def spawnCondor():
+    """Spawn dask client for condor cluster"""
     print("Trying to submit jobs to condor via dask!")
 
     cluster = LPCCondorCluster(ship_env=True)
     cluster.adapt(minimum=rs.MIN_WORKER, maximum=rs.MAX_WORKER)
     client = Client(cluster)
     print("One client created!")
+    print("===================================")
+    print(client)
+    return client
+
+def spawnLocal():
+    """Spawn dask client for local cluster"""
+    cluster = LocalCluster(processes=False, threads_per_worker=2)
+    cluster.adapt(minimum=0, maximum=6)
+    client = Client(cluster)
+    print("successfully created a dask client!")
     print("===================================")
     print(client)
     return client
