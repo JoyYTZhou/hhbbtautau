52bd21260d19a75b09cea47bedadbeeb62a24f17
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2024-02-14 21:12:46 -0500

    config update 2/10

--- scripts/jupsetup.sh
+++ scripts/jupsetup.sh
@@ -5,38 +5,18 @@
 # Takes one argument: Process name
 # Run this script only to open jupyter notebook
 # ===========================================================================================================
-OUTPUTPATH="/uscms_data/d1/joyzhou/output"
 
-if [ ! -z "$1" ]; then
-    export OUTPUTPATH=$OUTPUTPATH/$1
-    export PROCESS_NAME=$1
-else
-    export OUTPUTPATH=$OUTPUTPATH/all
-    export PROCESS_NAME=all
-fi
-
-echo "OUTPUTPATH has been set to: ${OUTPUTPATH}"
-echo "PROCESS_NAME has been set to: ${PROCESS_NAME}"
-
-if [ -d "$OUTPUTPATH" ]; then
-    echo "the directory $OUTPUTPATH exists."
-else
-    echo "the directory $OUTPUTPATH does not exist."
-    mkdir -pv $OUTPUTPATH
-fi
 
 export ENV_NAME=coffeajup
+export ENV_FOR_DYNACONF=LPCJUPYTER
+
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos7-gcc11-opt/setup.sh
 source ~/nobackup/${ENV_NAME}/bin/activate
 
-export HHBBTT=$PWD
 export PYTHONPATH=~/nobackup/${ENV_NAME}/lib/python3.9/site-packages:$PYTHONPATH
-export PYTHONPATH=$HHBBTT/src:$PYTHONPATH
-echo "HHBBTT has been set to: ${HHBBTT}"
-
+export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 source scripts/cleanpath.sh
-
 
 alias jup='jupyter notebook --no-browser --port=2001'
 
