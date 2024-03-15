996b858b50d1f7da19f28ed7fa16aa4a491cced0
Joy Zhou <joyzhou@cmslpc125.fnal.gov>
2024-03-15 13:30:53 -0500

    update on data

--- src/plot.py
+++ src/plot.py
@@ -1,14 +1,15 @@
-from analysis.plotutility import Visualizer
+from analysis.plotutility import *
 from config.selectionconfig import plotsetting as pltsetting
 
 def plot():
     vis = Visualizer(pltsetting)
-    vis.grepweights(output=True, from_raw=True)
-    vis.updatedir()
-    raw_df, wgt_df = vis.compute_allcf(lumi=5000, output=True)
-    vis.efficiency(raw_df, overall=True, append=False, save=True, save_name='rawcf')
-    vis.efficiency(wgt_df, overall=True, append=False, save=True, save_name='wgtcf')
-    vis.combine_roots(save=False, save_separate=True)
+    vis.getweights(from_load=True)
+    DataLoader.combine_roots(pltsetting, vis.wgt_dict)
+    # vis.updatedir()
+    # raw_df, wgt_df = vis.compute_allcf(lumi=5000, output=True)
+    # vis.efficiency(raw_df, overall=True, append=False, save=True, save_name='rawcf')
+    # vis.efficiency(wgt_df, overall=True, append=False, save=True, save_name='wgtcf')
+    # vis.combine_roots(save=False, save_separate=True)
 
 if __name__ == '__main__':
     plot() 
