bae047ebc905b9be79089d27fb7d3247afb46650
Joy Zhou <joyzhou@cmslpc102.fnal.gov>
2024-03-17 17:15:22 -0500

    tired 4/10

--- src/analysis/helper.py
+++ src/analysis/helper.py
@@ -58,7 +58,7 @@ def delfiles(dirname, pattern='*.root'):
 
 def filter_xrdfs_files(remote_dir, start_pattern, end_pattern, add_prefix=True):
     """Filter XRDFS files in a remote directory by a specific file ending."""
-    all_files = list_xrdfs_files(remote_dir, PREFIX)
+    all_files = list_xrdfs_files(remote_dir)
     if add_prefix:
         filtered_files = [PREFIX + "/" + f for f in all_files if f.split('/')[-1].startswith(start_pattern) and f.split('/')[-1].endswith(end_pattern)]
     else: 
