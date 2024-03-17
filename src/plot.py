e6706faa5614a75433fd9f2eca440330b48f0197
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2024-03-17 17:17:10 -0400

    small changes

--- src/plot.py
+++ src/plot.py
@@ -4,11 +4,11 @@ from config.selectionconfig import plotsetting as pltsetting
 def plot():
     vis = Visualizer(pltsetting)
     vis.getweights(save=True, from_raw=True)
-    DataLoader.combine_roots(pltsetting, vis.wgt_dict)
+    # DataLoader.combine_roots(pltsetting, vis.wgt_dict)
     # vis.updatedir()
-    # raw_df, wgt_df = vis.compute_allcf(lumi=5000, output=True)
-    # vis.efficiency(raw_df, overall=True, append=False, save=True, save_name='rawcf')
-    # vis.efficiency(wgt_df, overall=True, append=False, save=True, save_name='wgtcf')
+    raw_df, wgt_df = vis.compute_allcf(lumi=5000, output=True)
+    vis.efficiency(raw_df, overall=True, append=False, save=True, save_name='rawcf')
+    vis.efficiency(wgt_df, overall=True, append=False, save=True, save_name='wgtcf')
     # vis.combine_roots(save=False, save_separate=True)
 
 if __name__ == '__main__':
