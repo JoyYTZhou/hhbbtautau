a09b8c203d8d7cb1e33871db509de8a379506537
Joy Zhou <joyzhou@cmslpc139.fnal.gov>
2024-03-27 11:52:11 -0500

    update plotutil 1/10

--- src/analysis/plotutility.py
+++ src/analysis/plotutility.py
@@ -9,6 +9,7 @@ from utils.rootutil import load_fields
 from functools import wraps
 from utils.datautil import arr_handler, iterwgt
 from analysis.selutility import Object
+import awkward as ak
 
 class CFCombiner():
     """
@@ -51,10 +52,6 @@ class CFCombiner():
             checkpath(condorpath)
             transferfiles(self.outdir, condorpath)
         return raw_df, wgt_df
-
-    @property
-    def cleancfg(self):
-        return self._cleancfg
     
     def checkupdates(self):
         for ds in self.cleancfg.DATASETS:
@@ -65,15 +62,6 @@ class CFCombiner():
         if from_load:
             with open(pjoin(self.cleancfg.DATAPATH, 'wgt_total.json'), 'r') as f:
                 self.wgt_dict = json.load(f)        
-
-    def load_computed(self):
-        """Load all computed combined csv's for datasets in store"""
-        raw_pattern = pjoin(self.outdir, '*_cutflowraw.csv')
-        raw_df_list = load_csvs(raw_pattern)
-        wgt_pattern = pjoin(self.outdir, '*_cutflowwgt.csv')
-        wgt_df_list = load_csvs(wgt_pattern)
-
-        return raw_df_list, wgt_df_list
     
     def sort_cf(self, srcdir, save=True):
         """Create a multi index table that contains all channel cutflows for all datasets.
@@ -99,34 +87,38 @@ class CFCombiner():
 
         return allds_cf
         
+def iterdata(func):
+    @wraps(func)
+    def wrapper(instance, *args, **kwargs):
+        results = []
+        for process, dsitems in instance.data_dict.items():
+            if dsitems:
+                for ds in dsitems.keys():
+                    root_file = dsitems[ds]
+                    results.append(func(instance, root_file, process, ds, *args, **kwargs))
+        return results
+    return wrapper
+    
 class DataPlotter():
     def __init__(self, cleancfg, plotsetting):
         self.plotcfg = plotsetting
+        self.datadir = pjoin(cleancfg.LOCALOUTPUT, 'objlimited')
         with open(pjoin(cleancfg.DATAPATH, 'wgt_total.json'), 'r') as f:
             self.wgt_dict = json.load(f)
         self.data_dict = {}
         self.getdata()
-        self.datadir = pjoin(cleancfg.PLOTDATA, 'objlimited')
         self.resolution = cleancfg.RESOLUTION
-
-    @staticmethod
-    def iterdata(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            results = []
-            for process, dsitems in self.data_dict.items():
-                if dsitems:
-                    for ds in dsitems.keys():
-                        root_file = dsitems[ds]
-                        results.append(func(self, root_file, process, ds, *args, **kwargs))
-            return results
-        return wrapper
+        self.labels = self.getlabels()
+        self.wgt = self.getwgt()
 
     @iterwgt
     def getdata(self, process, ds):
-        rootfile = glob_files(self.datadir, startpattern=ds, endpattern='.root')[0]
-        if not process in self.data_dict.keys(): self.data_dict[process] = {}
-        else: self.data_dict[process][ds] = rootfile
+        result = glob_files(self.datadir, startpattern=ds, endpattern='.root')
+        if result:
+            rootfile = result[0]
+            if not process in self.data_dict.keys(): 
+                self.data_dict[process] = {}
+            if rootfile: self.data_dict[process][ds] = rootfile
     
     @iterdata
     def getobj(self, root_file, process, ds, obj_name):
@@ -139,19 +131,85 @@ class DataPlotter():
         return label
     
     @iterdata
-    def computewgt(self, root_file, process, ds, per_evt_wgt='Generator_weight', *args, **kwargs):
-        flat_wgt = self.wgt_dict[process][ds]
+    def getwgt(self, root_file, process, ds, per_evt_wgt='Generator_weight', lumi=5000, *args, **kwargs):
+        flat_wgt = self.wgt_dict[process][ds] * lumi
         wgt_arr = load_fields(root_file, tree_name='extra')[per_evt_wgt] * flat_wgt
         return wgt_arr
-        
-    def plotobj(self, evts, labels, wgt_arrs, objname, varname, indx):
-        obj_cfg = self.plotcfg[objname]
-        var_cfg = self.obj_cfg['var'][varname]
-        list_of_hists = [None] * len(evts)
-        for i, obj_arr in enumerate(evts):
-            var = DataPlotter.sortobj(obj_arr, sort_by=obj_cfg['sort_by'], sort_what=var)[indx]
-            list_of_hists[i] = self.deal_overflow(var, var_cfg['bins'], var_cfg['range'], weights=wgt_arrs[i])
     
+    @staticmethod
+    def concatevts(labels, objs, wgts):
+        concatedobjs = {}
+        concatedwgts = {}
+        for label, wgt, obj in zip(labels, wgts, objs):
+            if label in concatedobjs: 
+                concatedobjs[label].append(obj)
+                concatedwgts[label].append(wgt)
+            else:
+                concatedwgts[label] = [wgt]
+                concatedobjs[label] = [obj]
+        
+        objlist = []
+        wgtlist = []
+        lablist = []
+        for label in concatedobjs.keys():
+            objlist.append(ak.concatenate(concatedobjs[label], axis=0))
+            wgtlist.append(ak.concatenate(concatedwgts[label], axis=0))
+            lablist.append(label)
+
+        return objlist, wgtlist, lablist
+            
+    def savewgt(self):
+        """Save weighted selected events number to a csv."""
+        pass
+    
+    def plotobj(self, objname):
+        evts = self.getobj(objname)
+        objplotter = ObjectPlotter(objname, self.plotcfg[objname], self.wgt, self.labels, evts)
+    
+        
+class ObjectPlotter():
+    def __init__(self, objname, plotcfg, wgt, labels, evts):
+        self.objname = objname
+        self.objcfg = plotcfg
+        self.wgt = wgt
+        self.labels = labels
+        self.evts = evts
+    
+    def histobj(self, varname, objindx, bins_no, range, sort_by='pt'):
+        evts = self.evts
+        wgt_arrs = self.wgt
+        list_of_hists = [None] * len(evts)
+        bin_edges = None
+        for i, obj_arr in enumerate(evts):
+            var = ObjectPlotter.sortobj(obj_arr, sort_by=sort_by, sort_what=varname)[:,objindx]
+            if i==0:
+                list_of_hists[i], bin_edges = ObjectPlotter.deal_overflow(var, bins_no, range, weights=wgt_arrs[i])
+            else:
+                list_of_hists[i] = ObjectPlotter.deal_overflow(var, bins_no, range, weights=wgt_arrs[i])[0]
+        return list_of_hists, bin_edges
+    
+    @staticmethod
+    def plot_var(hist, bin_edges, legend, xlabel, range, save=True, title='', save_name='plot.png', **kwargs):
+        fig, ax = plt.subplots(figsize=(18, 10))
+        if title: ax.set_title(title)
+        hep.style.use("CMS")
+        hep.histplot(
+            hist,
+            bins=bin_edges,
+            histtype=kwargs.pop("histtype", 'step'),
+            ax=ax,
+            label=legend,
+            stack=kwargs.pop("stack", True),
+            **kwargs
+        )
+        ax.set_xlabel(xlabel, fontsize=15)
+        ax.set_ylabel("Events", fontsize=15)
+        ax.set_xlim(*range)
+        ax.legend(fontsize=15)
+        if save:
+            fig.savefig(save_name, dpi=300)
+        fig.show() 
+        
     @staticmethod
     def deal_overflow(arr, bins, range, weights=None):
         """Wrapper around numpy histogram function to deal with overflow.
@@ -161,40 +219,14 @@ class DataPlotter():
         - `bin_no`: number of bins
         - `range`: range of the histogram
         """
-        bins = np.linspace(*range, bins)
-        min_edge = bins[0]
-        max_edge = bins[-1]
+        if isinstance(bins, int):
+            bins = np.linspace(*range, bins)
+            min_edge = bins[0]
+            max_edge = bins[-1]
         adjusted_data = np.clip(arr, min_edge, max_edge)
         hist, bin_edges = np.histogram(adjusted_data, bins=bins, weights=weights)
         return hist, bin_edges
-
-    @staticmethod
-    def plot_var(hist, bin_edges, title, legend, xlabel, range, save=True, save_name='plot.png'):
-        fig, ax = plt.subplots(figsize=(10, 5))
-        ax.set_title(title)
-        hep.style.use("CMS")
-        hep.histplot(
-            hist,
-            bins=bin_edges,
-            histtype="fill",
-            alpha=0.5,
-            edgecolor="black",
-            ax=ax,
-            label=legend
-        )
-        ax.set_xlabel(xlabel, fontsize=15)
-        ax.set_ylabel("Events", fontsize=15)
-        ax.set_xlim(*range)
-        ax.legend()
-        if save:
-            fig.savefig(save_name, dpi=300)
-        fig.show() 
-
-class ObjectPlotter():
-    def __init__(self, objname, plotcfg):
-        self.objname = objname
-        self.plotcfg = plotcfg
-    
+            
     @staticmethod
     def sortobj(data, sort_by, sort_what, **kwargs):
         """Return an awkward array representation of the sorted attribute in data.
