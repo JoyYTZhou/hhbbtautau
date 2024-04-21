671cb254839e8eb763ee30979c439392bf745d3d
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-04-21 13:33:08 -0500

    endless upgrade

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-source scripts/lpcsetup.sh
 source scripts/venv.sh
 
 export PYTHONPATH=$PWD/src:$PYTHONPATH
-export PROCESS_NAME=ZH
+export PROCESS_NAME=ggF
 export DEBUG_MODE=true
+export ENV_FOR_DYNACONF=PRESELECT
 
 python3 src/main.py > testresult.log 2>&1 &
