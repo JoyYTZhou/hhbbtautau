e4066d04e0148055575e30b6dc9ecde2cddbe08c
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-04-22 15:14:54 -0500

    add eff

--- src/plot.py
+++ src/plot.py
@@ -2,21 +2,19 @@ from utils.rootutil import DataLoader
 from config.selectionconfig import cleansetting
 
 def postprocess():
-    dl = DataLoader(cleansetting)
+    dl = DataLoader()
     dl.hadd_cfs()
     dl.hadd_roots(cleancfg=cleansetting, wgt_dict=dl.wgt_dict)
 
 def getcf():
-    dl = DataLoader(cleansetting)
-    dl.weight_rawcf(dirbase='hadded')
-    dl.get_totraw(dirbase='hadded')
-    dl.get_totwgt()
+    dl = DataLoader()
+    dl.merge_cf()
 
 def getobj():
-    dl = DataLoader(cleansetting)
+    dl = DataLoader()
     dl.get_objs()
 
 if __name__ == '__main__':
     # postprocess()
-    getobj
+    getcf()
     
--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ export PROCESS_NAME=ggF
 export DEBUG_MODE=true
 export ENV_FOR_DYNACONF=PRESELECT
 
+rm -f testresult.log
 python3 src/main.py > testresult.log 2>&1 &
