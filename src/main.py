f6f732b12f6190bf7fd5b92fb6bce4a0401df1e5
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-04-29 13:25:36 -0500

    fix minimal

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 # UPDATE TIME: 2024-02-18
 # FROM JOY
-import os
-import time
+import os, time, gc
 
 PARENT_DIR = os.path.dirname(__file__) 
 from utils.filesysutil import checkx509
 from config.selectionconfig import dasksetting as dask_cfg
 from config.selectionconfig import runsetting as rs
 
-# if not rs.SPAWN_CONDOR: os.environ['CONDOR_CONFIG'] = os.path.join(PARENT_DIR, ".condor_config")
 
 def main():
+    gc.enable()
+    gc.set_debug(gc.DEBUG_LEAK)
+
     start_time = time.time()
     
     from analysis.spawndask import submitjobs
@@ -23,7 +24,6 @@ def main():
         print("Not spawning client explicitly!")
     
     checkx509()
-
     submitjobs(client)
     end_time = time.time()
     print(f"Execution time is {end_time - start_time} seconds")
