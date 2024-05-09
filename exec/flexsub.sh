901b7a4f1971b0fe2a436d01252167a20f16c396
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-05-09 11:22:34 -0500

    need to test new parallel

---
+++ exec/flexsub.sh
@@ -0,0 +1,32 @@
+PROCESS_NAME=$1
+
+source ../scripts/envutil.sh
+setup_dirname_local
+LPC_setup
+
+PROCESS_DATA=$DATA_DIR/preprocessed
+XRD_DIRECTOR=root://cmsxrootd.fnal.gov
+
+FILENAME=${PROCESS_DATA}/${PROCESS_NAME}.json
+INDX=$(jq 'length' $FILENAME)
+LEN=$((INDX-1))
+
+if [ "$PROCESS_NAME" == "TTbar" ]; then
+    cpusno=12
+    memory=24GB
+elif [ "$PROCESS_NAME" == "SingleH" ]; then
+    cpusno=8
+    memory=16GB
+else
+    cpusno=10
+    memory=20GB
+fi
+
+cp -f hhbbtt.sub runtime/hhbbtt_$PROCESS_NAME.sub 
+cat << EOF >> runtime/hhbbtt_$PROCESS_NAME.sub
+request_cpus = ${cpusno}
+request_memory = ${memory}
+request_disk = 5GB
+queue arg1 from $PROCESS_NAME
+queue arg2 from seq 0 ${LEN} |
+EOF
