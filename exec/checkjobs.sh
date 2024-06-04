ad14e83c5d22567fe9033b0ad2f2c78350bdd616
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-06-03 19:31:35 -0500

    checked jobs

--- exec/checkjobs.sh
+++ exec/checkjobs.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
 
+cd ..
+source scripts/venv.sh
+cd exec
+
+export PYTHONPATH=$PWD/src:$PYTHONPATH
+
+PROCESS_NAMES=("ZZ" "SingleH" "WW" "WZ" "WWZ" "WWW" "ggF" "DYJets" "TTbar" "ZH")
+
+for PROCESS_NAME in "${PROCESS_NAMES[@]}"; do
+    export PROCESS_NAME
+    echo "Checking $PROCESS_NAME"
+    python -c 'from analysis.spawndask import checkjobs; checkjobs()'
+    echo "=============================================================="
+done
     
