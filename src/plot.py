6b9da614aee346467efe6b264f68a2d020dddb21
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-06-11 01:53:13 -0500

    ready to hadd

--- src/plot.py
+++ src/plot.py
@@ -13,11 +13,11 @@ def getobj():
     dl = DataLoader()
     dl.get_objs()
 
-def programchoice():
+def programchoice() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Choose the post-processing options')
-    parser.add_argument(name='--postprocess', action='store_false', help='Execute hadding procedure for the specified, processed datasets')
-    parser.add_argument(name='--getcf', action='store_false', help='Get total cutflow table')
-    parser.add_argument(name='--getobj', action='store_false', help='Get ')
+    parser.add_argument('--postprocess', action='store_true', help='Execute hadding procedure for the specified, processed datasets')
+    parser.add_argument('--getcf', action='store_true', help='Get total cutflow table')
+    parser.add_argument('--getobj', action='store_true', help='Get delimited object files')
 
     args = parser.parse_args()
     if not any(vars(args).values()):
@@ -27,6 +27,8 @@ def programchoice():
     return args
 
 if __name__ == '__main__':
-    # postprocess()
-    getcf()
+    args = programchoice()
+    if args.postprocess: postprocess()
+    if args.getcf: getcf()
+    if args.getobj: getobj()
 
