35ac535a9aa5b714b06cd904ca2a2813508c5ab4
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-06-18 08:32:00 -0500

    filter on pd

--- src/config/plotsetting.py
+++ src/config/plotsetting.py
@@ -1,30 +1,17 @@
-import matplotlib as mpl
-
-colors = list(mpl.colormaps['Dark2'].colors).extend(list(mpl.colormaps['tab10'].colors))
-
 tau_pt = {
-    "LeadingPt": 
-        {'varname': 'pt',
-        'objindx': 0,
-        'bins': 135, 
-        'range': [0,300],
-        'xlabel': r'Leading $\tau$ $P_t$ (GeV)',
-        'save': True,
-        'alpha': 0.95,
-        'color': colors,
-        'stack': False,
-        'save_name': 'LeadingTauPt'},
-    "SubleadingPt": 
-        {'varname': 'pt',
-        'objindx': 1,
-        'bins': 135, 
-        'range': [0,300],
-        'xlabel': r'Subleading $\tau$ $P_t$ (GeV)',
-        'save': True,
-        'alpha': 0.95,
-        'color': colors,
-        'stack': False,
-        'save_name': 'SubleadingTauPt'}}
+    "LeadingTau_pt": {
+        "hist": {'bins': 135, 'range': [0,300]},
+        "plot": {'histtype': 'step',
+                'xlabel': r'Leading $\tau$ $P_t$ (GeV)',
+                'alpha': 0.95,
+                'stack': False}},
+    "SubleadingTau_pt": {
+        "hist": {'bins': 135, 'range': [0,300]},
+        "plot": {'xlabel': r'Subleading $\tau$ $P_t$ (GeV)',
+                'histtype': 'step',
+                'alpha': 0.95,
+                'stack': False}} 
+}
 
 tau_eta = {
     "LeadingEta": 
@@ -35,7 +22,6 @@ tau_eta = {
         'xlabel': r'Leading $\tau$ $\eta$',
         'save': True,
         'alpha': 0.95,
-        'color': colors,
         'stack': False,
         'save_name': 'LeadingTauEta'},
     "SubleadingEta": 
@@ -46,7 +32,6 @@ tau_eta = {
         'xlabel': r'Subleading $\tau$ $\eta$',
         'save': True,
         'alpha': 0.95,
-        'color': colors,
         'stack': False,
         'save_name': 'SubleadingTauEta'}}
 
@@ -61,7 +46,6 @@ jet_pt = {
         'xlabel': r'Leading Jet $P_t$ (GeV)',
         'save': True,
         'alpha': 0.95,
-        'color': colors,
         'stack': False,
         'save_name': 'LeadingJetPt'},
     "SubleadingPt": 
@@ -72,7 +56,6 @@ jet_pt = {
         'xlabel': r'Subleading Jet $P_t$ (GeV)',
         'save': True,
         'alpha': 0.95,
-        'color': colors,
         'stack': False,
         'save_name': 'SubleadingJetPt'}}
 
@@ -85,7 +68,6 @@ jet_btag = {
         'xlabel': r'Leading Jet B Tag Score',
         'save': True,
         'alpha': 0.95,
-        'color': colors,
         'stack': False,
         'save_name': 'LeadingJetBtag'},
     "SubleadingBtag": 
@@ -96,7 +78,6 @@ jet_btag = {
         'xlabel': r'Subleading Jet B Tag Score',
         'save': True,
         'alpha': 0.95,
-        'color': colors,
         'stack': False,
         'save_name': 'SubleadingJetBtag'}}
 
