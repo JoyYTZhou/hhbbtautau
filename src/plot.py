d24a0581de9198d66e8dd43caa8b8c4e5ffbd518
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-06-20 01:28:12 -0500

    training

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,6 @@
 from utils.rootutil import DataLoader
-from config.selectionconfig import cleansetting
+from analysis.plotutility import CSVPlotter
+from config.plotsetting import object_dict
 import argparse
 
 def postprocess():
@@ -17,12 +18,18 @@ def getobj():
     dl = DataLoader()
     dl.get_objs()
 
+def plotouts():
+    cp = CSVPlotter()
+    df = cp.postprocess(per_evt_wgt='Generator_weight_values')
+    cp.plot_hist(df, object_dict)
+
 def programchoice() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Choose the post-processing options')
     parser.add_argument('--postprocess', action='store_true', help='Execute hadding procedure for the specified, processed datasets')
     parser.add_argument('--checkouts', action='store_true', help='Check the output files')
     parser.add_argument('--getcf', action='store_true', help='Get total cutflow table')
     parser.add_argument('--getobj', action='store_true', help='Get delimited object files')
+    parser.add_argument('--plotouts', action='store_true', help='Plot stored options')
 
     args = parser.parse_args()
     if not any(vars(args).values()):
@@ -37,4 +44,5 @@ if __name__ == '__main__':
     if args.checkouts: checkouts()
     if args.getcf: getcf()
     if args.getobj: getobj()
+    if args.plotouts: plotouts()
 
