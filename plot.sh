1c8939c19028c878b848bda50567e39e1df664ae
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-06-30 00:38:34 -0500

    get this done

--- plot.sh
+++ plot.sh
@@ -4,7 +4,7 @@ rm -f plot.log
 
 # python3 src/plot.py --postprocess --getcf --plotouts > plot.log 2>&1 &
 # python3 src/plot.py --postprocess > plot.log 2>&1 &
-# python3 src/plot.py --getcf > plot.log 2>&1 &
-python3 src/plot.py --plotouts > plot.log 2>&1 &
+python3 src/plot.py --getcf > plot.log 2>&1 &
+# python3 src/plot.py --plotouts > plot.log 2>&1 &
 # python3 src/plot.py --checkouts > plot.log 2>&1 &
 
