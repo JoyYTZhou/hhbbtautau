efa0ac74166dd429ef2560b1771e91b34df6cf2f
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-07-05 01:45:33 -0500

    added QCD

--- src/data/datacollect.py
+++ src/data/datacollect.py
@@ -251,7 +251,7 @@ def produceCSV(datadir):
     
 if __name__ == "__main__":
     # query_MCsamples("data.json", "data_file.json", regex="NanoAODv")
-    add_weight("data_file.json", "preprocessed")
+    add_weight("data_file.json", "preprocessed", dsname=['SingleH', 'ZZ'])
     print("Jobs finished!")
     # produceCSV('preprocessed')
 
