b536ce7ff101f7f9b91659418045dd50b485f615
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2024-07-20 09:20:44 +0800

    fix truncation

--- src/plot.py
+++ src/plot.py
@@ -10,6 +10,9 @@ def postprocess():
 def mergecf():
     PostProcessor.merge_cf()
 
+def checkouts():
+    PostProcessor.check_roots()
+
 def plotouts():
     cp = CSVPlotter()
     df = cp.postprocess(per_evt_wgt='Generator_weight_values')
@@ -19,6 +22,7 @@ def programchoice() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Choose the post-processing options')
     parser.add_argument('--postprocess', action='store_true', help='Execute cutflow table merging')
     parser.add_argument('--mergecf', action='store_true', help='Execute hadding procedure for the specified, processed datasets')
+    parser.add_argument('--checkouts', action='store_true', help='Check the output root')
     parser.add_argument('--getobj', action='store_true', help='Get delimited object files')
     parser.add_argument('--plotouts', action='store_true', help='Plot stored options')
 
@@ -33,5 +37,6 @@ if __name__ == '__main__':
     args = programchoice()
     if args.postprocess: postprocess()
     if args.mergecf: mergecf()
+    if args.checkouts: checkouts()
     if args.plotouts: plotouts()
 
