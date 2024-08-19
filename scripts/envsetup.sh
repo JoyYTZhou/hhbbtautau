ec93f26786236aa32d7d5d6fb509fc96c50a3cc7
Joy Zhou <joyzhou@cmslpc307.fnal.gov>
2024-08-19 15:44:59 -0500

    passed concept test in dataset_tools

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -4,6 +4,7 @@
 # in the ~/nobackup area
 # ===========================================================================================================
 
+# NOTE: This script is meant for LCG set up with PYTHON<=3.10
 cd ~/work/hhbbtautau
 rm -r *.tar.gz
 cd ~/nobackup
@@ -13,14 +14,20 @@ echo "Please enter the name of the new virtual environment: "
 read ENV_NAME
 echo "=================================================================="
 
-python3 -m venv ${ENV_NAME}
+rm ${ENV_NAME}.tar.gz
+rm -r ${ENV_NAME}
+
+python -m venv ${ENV_NAME}
 echo "creating new venv..."
 source ${ENV_NAME}/bin/activate
 
-python3 -m pip install dynaconf --no-cache-dir
+python -m pip install coffea --upgrade --no-cache-dir
+echo "Installed coffea"
+python -m pip install dynaconf --no-cache-dir
 echo "Installed dyanconf"
-python3 -m pip install htcondor --no-cache-dir
+python -m pip install htcondor --no-cache-dir
 echo "Installed htcondor"
+python -m pip install rucio-clients==32 --no-cache-dir
 deactivate
 
 # create tarball
