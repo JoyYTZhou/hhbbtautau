33ee9e286bba1402d19461f55950e612a887f0e3
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2024-09-05 22:23:34 -0400

    update to testjob.py

--- tests/testjob.py
+++ tests/testjob.py
@@ -2,6 +2,7 @@ import unittest, os, glob
 
 from config.selectionconfig import runsetting as rs
 from src.analysis.spawnjobs import JobLoader
+import json
 
 pjoin = os.path.join
 
@@ -10,7 +11,7 @@ class TestLoader(unittest.TestCase):
         curr_dir = os.path.dirname(os.path.realpath(__file__))
         analysis_path = os.path.dirname(curr_dir)
         self.datapath = pjoin(analysis_path, "data/preprocessed")
-        self.outpath = pjoin(curr_dir, "testout")
+        self.outpath = pjoin(rs.OUTPUTDIR_PATH, "testout")
         self.jl = JobLoader(jobpath=self.outpath, datapath=self.datapath)
     
     def test_init(self):
@@ -27,6 +28,11 @@ class TestLoader(unittest.TestCase):
         if returned:
             files = glob.glob(pjoin(self.outpath, "*.json"))
             self.assertTrue(len(files) > 0, "No files were written!")
+            with open(files[0], 'r') as f:
+                data = json.load(f)
+                self.assertIn("files", data, 'Double check file generation in JobLoader.prepjobs')
+                self.assertIn("metadata", data, "Double check metadata generation in JobLoader.prepjobs")
+                self.assertEqual(len(data["files"]), 15, "Double check file division in JobLoader.prepjobs")
 
 if __name__ == '__main__':
     unittest.main()
