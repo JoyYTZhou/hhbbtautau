6568c76141ae36a8dd17ef55e9db42596dcadbd6
Joy Zhou <yuntongzhou@outlook.com>
2024-10-02 18:40:46 -0400

    Update postprocess configuration and script

--- postprocess.py
+++ postprocess.py
@@ -5,6 +5,20 @@ import argparse
 def __main__():
     parser = argparse.ArgumentParser()
     
+    parser.add_argument('--mode', choices=['hadd', 'yield'], required=True, 
+                        help='Choose the mode to run the postprocessor')
+    parser.add_argument('--group', type=str, nargs='+', required=False, default=None, 
+                        help='Group of the files to be hadded, e.g. DYJets TTbar etc.')
+    
+    args = parser.parse_args()
+    pp = PostProcessor(cleansetting, groups=args.group)
 
-    pp = PostProcessor(cleansetting)
-    pp.run()
+    if args.mode == 'hadd':
+        pp()
+        
+if __name__ == '__main__':
+    __main__()
+
+
+
+    
