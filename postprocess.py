54ad20b8516267d7f1b46b049d01396156459573
yuntongzhou@outlook.com <joyzhou@cmslpc305.fnal.gov>
2024-10-03 10:55:14 -0500

    update submodule

--- postprocess.py
+++ postprocess.py
@@ -5,13 +5,16 @@ import argparse
 def __main__():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--mode', choices=['hadd', 'yield'], required=True, 
+    parser.add_argument('--mode', choices=['check', 'hadd', 'yield'], required=True, 
                         help='Choose the mode to run the postprocessor')
     parser.add_argument('--group', type=str, nargs='+', required=False, default=None, 
                         help='Group of the files to be hadded, e.g. DYJets TTbar etc.')
     
     args = parser.parse_args()
     pp = PostProcessor(cleansetting, groups=args.group)
+
+    if args.mode == 'check':
+        pp.check_roots()
 
     if args.mode == 'hadd':
         pp()
