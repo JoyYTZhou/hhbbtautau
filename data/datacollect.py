3ee5afa6c944609223ce62a6401272a7972a23d1
Joy Zhou <yuntongzhou@outlook.com>
2024-10-21 08:55:34 -0400

    Refactor datacollect.py to improve query functionality and code organization

--- data/datacollect.py
+++ data/datacollect.py
@@ -7,25 +7,26 @@ from src.utils.filesysutil import FileSysHelper, pjoin
 
 class QueryRunner:
     """Class to run the query on the dataset and preprocess the data."""
-    def __init__(self) -> None:
+    def __init__(self, dataset) -> None:
         self.ddc = DataDiscoveryCLI()
         self.ddc.do_regex_sites(r"T[123]_(US)_\w+")
+        self.dataset = dataset
 
-    def __call__(self, dataset, infile='availableQuery.json', query_dir=None) -> None:
+    def __call__(self, infile='availableQuery.json', query_dir=None) -> None:
         with open(infile, 'r') as file:
             mcstrings = json.load(file)
         
         if query_dir is None:
-            self.query_from_dasgo(dataset, mcstrings)
+            self.query_from_dasgo(mcstrings)
         else:
             FileSysHelper.checkpath(query_dir, createdir=False, raiseError=True)
-            self.query_from_dir(dataset, query_dir, mcstrings)
+            self.query_from_dir(query_dir, mcstrings)
     
-    def query_from_dasgo(self, dataset, metaquery) -> None:
+    def query_from_dasgo(self, metaquery) -> None:
         """Query the available files from the DASGO."""
-        self.ddc.load_dataset_definition(dataset_definition=metaquery[dataset], query_results_strategy='all', replicas_strategy='manual')
+        self.ddc.load_dataset_definition(dataset_definition=metaquery[self.dataset], query_results_strategy='all', replicas_strategy='manual')
 
-        self.ddc.do_preprocess(output_file=dataset,
+        self.ddc.do_preprocess(output_file=self.dataset,
             step_size=10000,
             align_to_clusters=False,
             recalculate_steps=False,
@@ -35,20 +36,20 @@ class QueryRunner:
             allow_empty_datasets=True,
             scheduler_url=None)
         
-        shutil.move(f"{dataset}_available.json.gz", f"preprocessed/{dataset}.json.gz")
+        shutil.move(f"{self.dataset}_available.json.gz", f"preprocessed/{self.dataset}.json.gz")
     
-    def query_from_dir(self, dataset, query_dir, metaquery) -> None:
+    def query_from_dir(self, query_dir, metaquery) -> None:
         """Query the available files from the query_dir, e.g. a directory containing custom skim files. 
         Right now this does not do preprocessing."""
         queryed_result = {}
 
         pattern = re.compile(r'_(\d+)\.root$')
 
-        for datasetname in metaquery[dataset].keys():
+        for datasetname in metaquery[self.dataset].keys():
             queryed_result[datasetname] = {"files": {}}
-            queryed_result[datasetname]["metadata"] = metaquery[dataset][datasetname]
-            shortname = metaquery[dataset][datasetname]['shortname'] 
-            root_files = FileSysHelper.glob_files(pjoin(query_dir, dataset), f'{shortname}*.root')
+            queryed_result[datasetname]["metadata"] = metaquery[self.dataset][datasetname]
+            shortname = metaquery[self.dataset][datasetname]['shortname'] 
+            root_files = FileSysHelper.glob_files(pjoin(query_dir, self.dataset), f'{shortname}*.root')
             for root_file in root_files:
                 match = pattern.search(root_file)
                 if match:
@@ -57,18 +58,19 @@ class QueryRunner:
         
         FileSysHelper.checkpath('skimmed', createdir=True)
 
-        with gzip.open(f"skimmed/{dataset}.json.gz", 'wt') as file:
+        with gzip.open(f"skimmed/{self.dataset}.json.gz", 'wt') as file:
             json.dump(queryed_result, file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run preprocessor on fileset')    
-    parser.add_argument('-d', '--dataset', type=str, required=True, help='dataset to run program on')
+    parser.add_argument('-d', '--dataset', type=str, required=True, 
+                        help='group name of the dataset to run program on, e.g. TTbar, DYJets, etc. Note that this must match the key in the availableQuery.json file.')
     parser.add_argument('-s', '--skip', action='store_true', required=False, help='whether to skip preprocess.')
     parser.add_argument('-q', '--query', type=str, required=False, default=None, help='directory containing custom skim.')
 
     args = parser.parse_args()
-    qr = QueryRunner()
+    qr = QueryRunner(args.dataset)
     if args.skip:
-        qr.dump_query(args.dataset)
+        qr.dump_query()
     else:
-        qr(args.dataset, query_dir=args.query)
+        qr(query_dir=args.query)
