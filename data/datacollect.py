c0b9bf5212fb15a30cf5eabe11e9de575da61664
Joy Zhou <yuntongzhou@outlook.com>
2024-10-22 15:25:16 -0400

    Refactor datacollect.py to improve query functionality and code organization

--- data/datacollect.py
+++ data/datacollect.py
@@ -6,27 +6,35 @@ from coffea.dataset_tools.dataset_query import DataDiscoveryCLI
 from src.utils.filesysutil import FileSysHelper, pjoin
 
 class QueryRunner:
-    """Class to run the query on the dataset and preprocess the data."""
+    """Class to run the query on dataset strings and preprocess the dataset.
+    Currently only supports MC datasets. Dependent on DataDiscoveryCLI from coffea."""
     def __init__(self, dataset) -> None:
+        """Initialize the QueryRunner object.
+        
+        Parameters
+        - `dataset`: str, the dataset name key in the json file to query and preprocess."""
         self.ddc = DataDiscoveryCLI()
         self.ddc.do_regex_sites(r"T[123]_(US)_\w+")
         self.dataset = dataset
 
-    def __call__(self, infile='availableQuery.json', query_dir=None) -> None:
-        with open(infile, 'r') as file:
-            mcstrings = json.load(file)
-        
-        if query_dir is None:
-            self.query_from_dasgo(mcstrings)
-        else:
-            FileSysHelper.checkpath(query_dir, createdir=False, raiseError=True)
-            self.query_from_dir(query_dir, mcstrings)
+    def __call__(self, indir='availableMC', query_dir=None) -> None:
+        """Run the query on the dataset and preprocess the dataset."""
+        for infile in FileSysHelper.glob_files(indir, '*.json'):
+            with open(infile, 'r') as file:
+                mcstrings = json.load(file)
+            
+            name = infile.split('/')[-1].split('.')[0]
+            if query_dir is None:
+                self.query_from_dasgo(mcstrings, suffix=name)
+            else:
+                FileSysHelper.checkpath(query_dir, createdir=False, raiseError=True)
+                self.query_from_dir(query_dir, mcstrings)
     
-    def query_from_dasgo(self, metaquery) -> None:
-        """Query the available files from the DASGO."""
+    def query_from_dasgo(self, metaquery, suffix) -> None:
+        """Query the available files from the DASGO. Produce a json.gz file with the query results (files, redirectors, uuids etc.)"""
         self.ddc.load_dataset_definition(dataset_definition=metaquery[self.dataset], query_results_strategy='all', replicas_strategy='manual')
 
-        self.ddc.do_preprocess(output_file=self.dataset,
+        self.ddc.do_preprocess(output_file=f'{self.dataset}_{suffix}',
             step_size=10000,
             align_to_clusters=False,
             recalculate_steps=False,
@@ -36,11 +44,15 @@ class QueryRunner:
             allow_empty_datasets=True,
             scheduler_url=None)
         
-        shutil.move(f"{self.dataset}_available.json.gz", f"preprocessed/{self.dataset}.json.gz")
+        shutil.move(f"{self.dataset}_{suffix}_available.json.gz", f"preprocessed/{self.dataset}_{suffix}.json.gz")
     
     def query_from_dir(self, query_dir, metaquery) -> None:
         """Query the available files from the query_dir, e.g. a directory containing custom skim files. 
-        Right now this does not do preprocessing."""
+        Right now this does not do preprocessing.
+        
+        Parameters
+        - `query_dir`: str, the directory containing the custom skim files (currently only supports root files).
+        - `metaquery`: dict, the metaquery dictionary containing the dataset information."""
         queryed_result = {}
 
         pattern = re.compile(r'_(\d+)\.root$')
