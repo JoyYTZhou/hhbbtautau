9dcb49208a01fad4dcf6a96245d96ddadcbeb402
Joy Zhou <yuntongzhou@outlook.com>
2024-10-25 13:38:01 -0500

    Refactor jobsub.sh to include year in genjobs.py command Refactor datacollect.py to remove corrupted files and update runsetting.toml Update genjobs.py to fix a typo in the argument name Update availableMC/2023Summer.json to fix shortnames of TTbar samples Add ZZZ_2022PostEE.json.gz to preprocessed data Add ZH_2022PostEE.json.gz to preprocessed data

--- exec/genjobs.py
+++ exec/genjobs.py
@@ -7,7 +7,7 @@ cwd = os.getcwd()
 projectbase = os.path.dirname(cwd)
 
 def gen_jobs(groupname):
-    jl = JobLoader(datapath=pjoin(projectbase, 'data', rs.JOB_PATH), groupname=groupname, jobpath=pjoin(cwd, rs.JOB_DIRNAME),
+    jl = JobLoader(datapath=pjoin(projectbase, 'data', rs.JOB_PATH), kwd=groupname, jobpath=pjoin(cwd, rs.JOB_DIRNAME),
                    transferPBase=rs.TRANSFER_PATH, out_endpattern=rs.get('OUTENDPATTERN', [".root", "cutflow.csv"]))
     jl.writejobs()
 
