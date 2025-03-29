2311986858fa358bf9dec37861a6f77a663c3c8d
Joy Zhou <yuntongzhou@outlook.com>
2025-03-29 14:58:42 -0400

    Enhance QueryRunner to include output path parameter and improve dataset querying logic

--- data/datacollect.py
+++ data/datacollect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # adapted from: https://github.com/bu-cms/bucoffea/blob/83daf25146d883df5131d0b50a51c0a6512d7c5f/bucoffea/helpers/dasgowrapper.py
 
-import json, shutil, argparse, re, gzip
+import json, shutil, argparse, re, gzip, logging
 from coffea.dataset_tools.dataset_query import DataDiscoveryCLI
 from src.utils.filesysutil import FileSysHelper, pjoin
 
@@ -11,7 +11,7 @@ data_dir = "availableData"
 class QueryRunner:
     """Class to run the query on dataset strings and preprocess the dataset.
     Currently only supports MC datasets. Dependent on DataDiscoveryCLI from coffea."""
-    def __init__(self, dataset, year, is_mc) -> None:
+    def __init__(self, dataset, year, is_mc, out_path) -> None:
         """Initialize the QueryRunner object.
         
         Parameters
@@ -47,8 +47,11 @@ class QueryRunner:
             for year, filename in json_files.items()
         }
 
-        # Set datasets to process
-        self.dataset = [dataset] if dataset else list(self.mcstrings.keys())
+        self.dataset = dataset
+
+        self.outpath = out_path
+        
+        FileSysHelper.checkpath(self.outpath, createdir=True)
 
     @staticmethod
     def _load_json(filepath):
@@ -62,47 +65,80 @@ class QueryRunner:
             self.query_from_dasgo()
         else:
             FileSysHelper.checkpath(query_dir, createdir=False, raiseError=True)
-            for dataset in self.dataset:
-                if FileSysHelper.checkpath(pjoin(query_dir, self.year, dataset), createdir=False, raiseError=False):
-                    self.query_from_dir(query_dir, dataset, self.year)
+            for year, year_data in self.mcstrings.items():
+                year_dir = pjoin(query_dir, year)
+                if FileSysHelper.checkpath(year_dir, createdir=False, raiseError=False):
+                    for dataset in year_data.keys():
+                        if FileSysHelper.checkpath(pjoin(year_dir, dataset), createdir=False, raiseError=False):
+                            self.query_from_dir(query_dir, dataset, year, self.outpath)
+                        else:
+                            logging.warning(f"No custom skims for {year} {dataset} have been produced.")
                 else:
-                    print(f"No custom skims for {self.year} {dataset} have been produced.")
+                    logging.warning(f"No custom skims for {year} have been produced.")
     
     def __add_MC_meta(self):
-        if self._isMC:
-            for dataset in self.dataset:
-                for datasetname in self.mcstrings[dataset].keys():
-                    self.mcstrings[dataset][datasetname]["is_mc"] = True
+        """Add 'is_mc' metadata flag to MC datasets.
+        If self.dataset is specified, only add to that dataset, otherwise add to all datasets."""
+        if not self._isMC:
+            return
+
+        # Iterate through all years
+        for year, year_data in self.mcstrings.items():
+            # If dataset is specified, only process that dataset
+            datasets_to_process = [self.dataset] if self.dataset else year_data.keys()
+
+            # Process each dataset
+            for dataset in datasets_to_process:
+                # Skip if dataset doesn't exist in this year
+                if dataset not in year_data:
+                    continue
+
+                # Add is_mc flag to each dataset name entry
+                for datasetname in year_data[dataset]:
+                    year_data[dataset][datasetname]["is_mc"] = True
+
     
     def query_from_dasgo(self) -> None:
         """Query the available files from the DASGO. Produce a json.gz file with the query results (files, redirectors, uuids etc.)"""
-        suffix = self.year
         self.__add_MC_meta()
-        
-        for dataset in self.dataset:
-            self.ddc.load_dataset_definition(dataset_definition=self.mcstrings[dataset], query_results_strategy='all', replicas_strategy='manual')
-    
-        if not self._isMC:
-            out_name = f"Data_{suffix}"
-        else:
-            if len(self.dataset) > 1:
-                out_name = f'{suffix}'
+        for year, year_data in self.mcstrings.items():
+            suffix = year
+            
+            if dataset is not None:
+                # If dataset is specified, only process that dataset
+                datasets_to_process = [self.dataset]
             else:
-                out_name = f'{self.dataset[0]}_{suffix}'
-
-        self.ddc.do_preprocess(output_file=out_name,
-            step_size=80000,
-            align_to_clusters=False,
-            recalculate_steps=False,
-            files_per_batch=1,
-            file_exceptions=(OSError, IndexError),
-            save_form=False,
-            allow_empty_datasets=True,
-            scheduler_url=None)
-        
-        shutil.move(f"{out_name}_available.json.gz", f"preprocessed/{out_name}.json.gz")
+                # Process all datasets in this year
+                datasets_to_process = year_data.keys()
+            
+            # Process each dataset
+            for dataset in datasets_to_process:
+                self.ddc.load_dataset_definition(dataset_definition=self.mcstrings[dataset], query_results_strategy='all', replicas_strategy='manual')
     
-    def query_from_dir(self, query_dir, dataset, year) -> None:
+            if not self._isMC:
+                out_name = f"Data_{suffix}"
+            else:
+                if len(self.dataset) > 1:
+                    out_name = f'{suffix}'
+                else:
+                    out_name = f'{self.dataset[0]}_{suffix}'
+
+            self.ddc.do_preprocess(output_file=out_name,
+                step_size=80000,
+                align_to_clusters=False,
+                recalculate_steps=False,
+                files_per_batch=1,
+                file_exceptions=(OSError, IndexError),
+                save_form=False,
+                allow_empty_datasets=True,
+                scheduler_url=None)
+            
+            shutil.move(f"{out_name}_available.json.gz", f"preprocessed/{out_name}.json.gz")
+        
+            self.ddc = DataDiscoveryCLI()
+            self.ddc.do_regex_sites(r"T[123]_(US)_\w+")
+    
+    def query_from_dir(self, query_dir, dataset, year, outpath) -> None:
         """Query the available files from the query_dir, e.g. a directory containing custom skim files. 
         Right now this does not do preprocessing.
         
@@ -144,6 +180,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--skip', action='store_true', required=False, help='whether to skip preprocess.')
     parser.add_argument('-q', '--query', type=str, required=False, default=None, help='directory containing custom skim.')
     parser.add_argument('--is_mc', action='store_true', help='specify if processing Monte Carlo samples (if set) or collision data (if not set)')
+    parser.add_argument('-o', '--outpath', type=str, required=False, default='skimmed', help='output path for collecting skimmed data')
     args = parser.parse_args()
 
     qr = QueryRunner(args.dataset, args.year, is_mc=args.is_mc)
